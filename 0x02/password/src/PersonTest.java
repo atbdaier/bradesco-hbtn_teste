b58702bf8cbe8dc764f21d7e2a0045c29713e92c
Andre Daier <andre.daier@bradesco.com.br>
2025-10-18 00:43:17 -0300

    Task Password

---
+++ 0x02/password/src/PersonTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PersonTest {
+
+    private static Person person;
+
+    @BeforeAll
+    public static void setup() {
+         person = new Person();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    void check_user_valid(String username) {
+        Assertions.assertTrue(person.checkUser(username));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+    void check_user_not_valid(String username) {
+        Assertions.assertFalse(person.checkUser(username));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    void does_not_have_letters(String password) {
+        Assertions.assertFalse(person.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    void does_not_have_numbers(String password) {
+        Assertions.assertFalse(person.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
+    void does_not_have_eight_chars(String password) {
+        Assertions.assertFalse(person.checkPassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    void check_password_valid(String password) {
+        Assertions.assertTrue(person.checkPassword(password));
+    }
+
+
+}
